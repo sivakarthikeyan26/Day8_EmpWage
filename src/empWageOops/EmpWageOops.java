e4c8c9f8216597a32816eda4fda2c37e8fa19a2a
Your Name <you@example.com>
2022-06-29 18:37:51 +0530

    wage till condition is calculated

--- src/empWageOops/EmpWageOops.java
+++ src/empWageOops/EmpWageOops.java
@@ -1,13 +1,29 @@
 package empWageOops;
 
-public class EmpWageOops {
+import java.util.Scanner;
 
+public class EmpWageOops {
+	public static final Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		Emp_compute emp = new Emp_compute();
-		emp.present_absent();
-		emp.daily_wage();
-		emp.monthly_wage();
-
+		while(true) {
+			System.out.println("\n1.For computing whether the employee is absent or present\n2.For computing employee daily wage\n3.For computing employee monthly wage\n4.For computing employee wage till conditiion");
+			int choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				emp.present_absent();
+				break;
+			case 2:
+				emp.daily_wage();
+				break;
+			case 3:
+				emp.monthly_wage();
+				break;
+			case 4:
+				emp.wage_till_condition();
+				break;
+			}
+		}
 	}
 
 }
--- src/empWageOops/Emp_compute.java
+++ src/empWageOops/Emp_compute.java
@@ -6,32 +6,38 @@ public class Emp_compute {
 	int full_day_hour = 8;
 	int part_time_hour = 4;
 	double emp_check;
+
 	public Emp_compute() {
-		emp_check = Math.floor(Math.random() * 10) % 3+1;
+		emp_check = Math.floor(Math.random() * 10) % 3 + 1;
 	}
+
 	public void present_absent() {
 		System.out.println("welcome to employee wage computation");
 		if (emp_check == emp_present) {
 			System.out.println("The employee is present");
 
-		} else if(emp_check==2) {
+		} else if (emp_check == 2) {
 			System.out.println("the employee is absent");
 		}
+		else {
+			System.out.println("The employee is present but he is part time");
+		}
 	}
+
 	public void daily_wage() {
 		if (emp_check == emp_present) {
 			System.out.println("The wage for a day is : " + wage_per_hour * full_day_hour);
 		} else if (emp_check == 2) {
 			System.out.println("the employee is absent so no wage");
-		}
-		else {
+		} else {
 			System.out.println("The employee is part time");
-			System.out.println("the employee wage per day is : " +wage_per_hour*part_time_hour);
+			System.out.println("the employee wage per day is : " + wage_per_hour * part_time_hour);
 		}
 	}
+
 	public void monthly_wage() {
 		int month = 20;
-		switch ((int)emp_check) {
+		switch ((int) emp_check) {
 		case 1:
 			System.out.println("The wage for the month is : " + wage_per_hour * full_day_hour * month);
 			break;
@@ -41,5 +47,26 @@ public class Emp_compute {
 		case 2:
 			System.out.println("the employee is absent no wage");
 		}
+	}
+
+	public void wage_till_condition() {
+		int day_count = 1;
+		int hour_count = 0;
+		while (day_count != 20 && hour_count != 100) {
+			int emp_check = (int) (Math.floor(Math.random() * 10) % 3+1);
+			switch (emp_check) {
+			case 1:
+				day_count += 1;
+				hour_count += full_day_hour;
+				break;
+			case 3:
+				day_count += 1;
+				hour_count += part_time_hour;
+				break;
+			default:
+				continue;
+			}
+		}
+		System.out.println("The employee wage with condition is : " + wage_per_hour * hour_count * day_count);
 	}
 }
