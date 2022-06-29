99a8fe05b5119d947689cdba41d6fc598888cf2f
Your Name <you@example.com>
2022-06-29 17:48:06 +0530

    employee daily wage is calculated

--- src/empWageOops/EmpWageOops.java
+++ src/empWageOops/EmpWageOops.java
@@ -5,6 +5,7 @@ public class EmpWageOops {
 	public static void main(String[] args) {
 		Emp_compute emp = new Emp_compute();
 		emp.present_absent();
+		emp.daily_wage();
 
 	}
 
--- src/empWageOops/Emp_compute.java
+++ src/empWageOops/Emp_compute.java
@@ -1,13 +1,27 @@
 package empWageOops;
 
 public class Emp_compute {
+	int emp_present = 1;
+	double emp_check;
+	public Emp_compute() {
+		emp_check = Math.floor(Math.random() * 10) % 2;
+	}
 	public void present_absent() {
-		int emp_present = 1;
 		System.out.println("welcome to employee wage computation");
-		double emp_check = Math.floor(Math.random() * 10) % 2;
 		if (emp_check == emp_present) {
 			System.out.println("The employee is present");
 
+		} else {
+			System.out.println("the employee is absent");
+		}
+	}
+	public void daily_wage() {
+		int wage_per_hour = 20;
+		int full_day_hour = 8;
+		System.out.println("welcome to employee wage computation");
+		if (emp_check == emp_present) {
+			System.out.println("The employee is present");
+			System.out.println("The wage is : " + wage_per_hour * full_day_hour);
 		} else {
 			System.out.println("the employee is absent");
 		}
