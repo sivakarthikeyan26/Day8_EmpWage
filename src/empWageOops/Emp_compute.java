a6393c45f11080ed8ed2d0e1a68ffdf2d4153cea
Your Name <you@example.com>
2022-06-29 17:56:49 +0530

    part time wage is included

--- src/empWageOops/Emp_compute.java
+++ src/empWageOops/Emp_compute.java
@@ -4,26 +4,29 @@ public class Emp_compute {
 	int emp_present = 1;
 	double emp_check;
 	public Emp_compute() {
-		emp_check = Math.floor(Math.random() * 10) % 2;
+		emp_check = Math.floor(Math.random() * 10) % 3+1;
 	}
 	public void present_absent() {
 		System.out.println("welcome to employee wage computation");
 		if (emp_check == emp_present) {
 			System.out.println("The employee is present");
 
-		} else {
+		} else if(emp_check==2) {
 			System.out.println("the employee is absent");
 		}
 	}
 	public void daily_wage() {
 		int wage_per_hour = 20;
 		int full_day_hour = 8;
-		System.out.println("welcome to employee wage computation");
+		int part_time_hour = 4;
 		if (emp_check == emp_present) {
-			System.out.println("The employee is present");
 			System.out.println("The wage is : " + wage_per_hour * full_day_hour);
-		} else {
-			System.out.println("the employee is absent");
+		} else if (emp_check == 2) {
+			System.out.println("the employee is absent so no wage");
+		}
+		else {
+			System.out.println("The employee is part time");
+			System.out.println("the employee wage is : " +wage_per_hour*part_time_hour);
 		}
 	}
 }
